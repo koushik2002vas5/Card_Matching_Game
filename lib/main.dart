196c8f82f472685152d80c8f4d76acdc5371e490
koushik2002vas5 <koushiksai.vas5@gmail.com>
2025-03-13 00:46:22 -0400

    Implemented Card Flipping Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 void main() => runApp(const MyApp());
 
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Card Matching Game',
-      theme: ThemeData.dark(), // Dark theme
+      theme: ThemeData.dark(),
       home: const CardMatchingGame(),
     );
   }
@@ -41,44 +40,54 @@ class _CardMatchingGameState extends State<CardMatchingGame> {
   late List<CardModel> cards;
   late List<int> selectedCards;
   late bool isBusy;
-  late Timer timer;
   int score = 0;
-  int timeElapsed = 0;
-
   final int gridSize = 4;
 
   @override
   void initState() {
     super.initState();
     initializeGame();
-    startTimer();
+    selectedCards = [];
+    isBusy = false;
   }
 
   void initializeGame() {
-    List<String> icons = [
-      '🚗',
-      '🚕',
-      '🚌',
-      '🚓',
-      '🚑',
-      '🚒',
-      '🚜',
-      '🚛',
-      '🚎',
-      '🚚',
-      '🏎️',
-      '🚀'
-    ];
-    int totalPairs = (gridSize * gridSize) ~/ 2;
-    if (icons.length < totalPairs) {
-      throw Exception('Not enough icons to populate the grid.');
-    }
+    List<String> icons = ['🚗', '🚕', '🚌', '🚓', '🚑', '🚒', '🚜', '🚛'];
     cards = [];
-    for (int i = 0; i < totalPairs; i++) {
+    for (int i = 0; i < icons.length; i++) {
       cards.add(CardModel(front: icons[i], back: '❓'));
       cards.add(CardModel(front: icons[i], back: '❓'));
     }
     cards.shuffle();
-    selectedCards = [];
-    isBusy = false;
   }
+
+  Future<void> flipCard(int index) async {
+    if (isBusy || cards[index].isFaceUp || cards[index].isMatched) return;
+
+    setState(() {
+      cards[index].isFaceUp = true;
+    });
+
+    selectedCards.add(index);
+
+    if (selectedCards.length == 2) {
+      isBusy = true;
+      await Future.delayed(const Duration(seconds: 1));
+      if (cards[selectedCards[0]].front != cards[selectedCards[1]].front) {
+        setState(() {
+          cards[selectedCards[0]].isFaceUp = false;
+          cards[selectedCards[1]].isFaceUp = false;
+          score -= 5;
+        });
+      } else {
+        setState(() {
+          cards[selectedCards[0]].isMatched = true;
+          cards[selectedCards[1]].isMatched = true;
+          score += 10;
+        });
+      }
+      selectedCards.clear();
+      isBusy = false;
+    }
+  }
+}
